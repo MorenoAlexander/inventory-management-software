ffa83e9f029033cccfd1c06d97f1b551336bdf1c
Alexander Moreno <am@alexandermoreno.com>
2023-02-01 03:24:02 -0600

    added configuration for oracle sql + swagger config

--- Inventory-Management-Software.Infrastructure/ConfigureDataServices.cs
+++ Inventory-Management-Software.Infrastructure/ConfigureDataServices.cs
@@ -1,16 +1,21 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Oracle.ManagedDataAccess.Client;
 
 namespace Inventory_Management_Software.Infrastructure;
 
 public static class ConfigureDataServices
 {
-    public static void AddDataServices(this IServiceCollection serviceCollection, string connectionString)
+    public static void AddDataServices(this IServiceCollection serviceCollection, IConfiguration config)
     {
         serviceCollection.AddDbContext<GenericIMSContext>(options =>
         {
 
-            options.UseOracle(connectionString);
+            options.UseOracle(config.GetConnectionString("Default"));
+                OracleConfiguration.TnsAdmin = config.GetSection("OracleConfiguration")["TnsAdmin"];
+            OracleConfiguration.WalletLocation = config.GetSection("OracleConfiguration")["WalletLocation"];
+            
         });
     }
 }
