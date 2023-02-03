cc25292462637fc51c4c258a9869c9640e67b06f
Alexander Moreno <am@alexandermoreno.com>
2023-02-02 23:20:00 -0600

    added precision for cost fields

--- Inventory-Management-Software.Infrastructure/Data/Config/ProductConfig.cs
+++ Inventory-Management-Software.Infrastructure/Data/Config/ProductConfig.cs
@@ -13,6 +13,10 @@ public class ProductConfig : IEntityTypeConfiguration<Product>
         builder.Property(p => p.Description).HasMaxLength(1024);
         builder.Property(p => p.SKU).HasMaxLength(256);
         builder.HasIndex(p => p.SKU).IsUnique();
+        builder.Property(p => p.Price).HasPrecision(18, 2);
+        builder.Property(p => p.Cost).HasPrecision(18, 2);
+        builder.Property(p => p.ManufacturerSuggestedRetailPrice).HasPrecision(18, 2);
+        
 
         builder.HasOne<ProductCategory>(p => p.ProductCategory).WithMany(p => p.Products)
             .HasForeignKey(p => p.ProductCategoryId);
