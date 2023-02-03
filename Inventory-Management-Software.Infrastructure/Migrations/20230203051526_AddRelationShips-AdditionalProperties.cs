54c3944adccb61f6ef5833c414f6564147c1f1c8
Alexander Moreno <am@alexandermoreno.com>
2023-02-02 23:19:46 -0600

    added migration

---
+++ Inventory-Management-Software.Infrastructure/Migrations/20230203051526_AddRelationShips-AdditionalProperties.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManagementSoftware.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelationShipsAdditionalProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Cost",
+                table: "Products",
+                type: "DECIMAL(18, 2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "NVARCHAR2(2000)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ManufacturerSuggestedRetailPrice",
+                table: "Products",
+                type: "DECIMAL(18, 2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "DECIMAL(18, 2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SKU",
+                table: "Products",
+                type: "NVARCHAR2(2000)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cost",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ManufacturerSuggestedRetailPrice",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "SKU",
+                table: "Products");
+        }
+    }
+}
