5dc5d0eb66ac34acabbbfe3d0599159478064d05
Alexander Moreno <am@alexandermoreno.com>
2023-02-02 23:25:15 -0600

    added migrations for decimal properties

---
+++ Inventory-Management-Software.Infrastructure/Migrations/20230203052105_AddProductDecimalPropertyPrecision.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManagementSoftware.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductDecimalPropertyPrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "DECIMAL(18, 2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "DECIMAL(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ManufacturerSuggestedRetailPrice",
+                table: "Products",
+                type: "DECIMAL(18, 2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "DECIMAL(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Cost",
+                table: "Products",
+                type: "DECIMAL(18, 2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "DECIMAL(18,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "DECIMAL(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "DECIMAL(18, 2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ManufacturerSuggestedRetailPrice",
+                table: "Products",
+                type: "DECIMAL(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "DECIMAL(18, 2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Cost",
+                table: "Products",
+                type: "DECIMAL(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "DECIMAL(18, 2)");
+        }
+    }
+}
