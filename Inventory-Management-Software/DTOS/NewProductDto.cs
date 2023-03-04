48936a681d65d5a5fd2f188ed8530965a0a07252
Alexander Moreno <am@alexandermoreno.com>
2023-03-04 12:25:11 -0600

    added NewProductDto.cs; updated ProductController.cs

---
+++ Inventory-Management-Software/DTOS/NewProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Inventory_Management_Software.DTOS {
+    public class NewProductDto {
+        public string SKU { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
+        public decimal MSRP { get; set; }
+
+    }
+}
