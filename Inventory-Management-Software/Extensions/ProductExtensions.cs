2392dceaa7241995b96fc958d8b0158d7abfff44
Alexander Moreno <am@alexandermoreno.com>
2023-03-04 12:24:47 -0600

    added UpdateFromDto for products

---
+++ Inventory-Management-Software/Extensions/ProductExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Inventory_Management_Software.Core.Entities;
+using Inventory_Management_Software.DTOS;
+
+namespace Inventory_Management_Software.Extensions;
+    public static class ProductExtensions {
+
+        public static void UpdateFromDto(this Product product, NewProductDto dto)
+        {
+            product.SKU = dto.SKU;
+            product.Name = dto.Name;
+            product.Description = dto.Description;
+            product.Price = dto.Price;
+            product.Cost = dto.Cost;
+            product.ManufacturerSuggestedRetailPrice = dto.MSRP;
+        } 
+    }
